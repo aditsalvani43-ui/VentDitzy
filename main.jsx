f23461338a3c43a4134557584cfd072bb714350b
aditsalvani43-ui <aditsalvani26@gmail.com>
2025-09-15 16:23:32 +0700

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,12 @@
+```jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import BratVortexGenerator from './BratVortexGenerator';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BratVortexGenerator />
+  </React.StrictMode>
+);
+```
